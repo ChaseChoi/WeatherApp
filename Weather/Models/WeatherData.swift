d4e888304bdf5dc3d09584d45a22faaf3bce45b8
ChaseChoi <caiguihao@gmail.com>
2020-08-15 21:28:11 +0800

    Decode JSON data

--- Weather/Models/WeatherData.swift
+++ Weather/Models/WeatherData.swift
@@ -18,12 +18,13 @@ struct WeatherData {
     let temperature: Double
     let iconName: String
     
-//    let dailyData: [WeatherData]
+    let dailyData: [DailyWeatherData]
     
-    enum TopLevelCodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case latitude
         case longitude
         case currently
+        case dailyData = "daily"
     }
     
     enum CurrentlyWeatherCodingKeys: String, CodingKey {
@@ -32,12 +33,17 @@ struct WeatherData {
         case temperature
         case iconName = "icon"
     }
+    
+    enum DailyWeatherCodingKeys: String, CodingKey {
+        case data
+    }
 }
 
 extension WeatherData: Decodable {
     init(from decoder: Decoder) throws {
-        let topLevelValues = try decoder.container(keyedBy: TopLevelCodingKeys.self)
+        let topLevelValues = try decoder.container(keyedBy: CodingKeys.self)
         let currentlyWeather = try topLevelValues.nestedContainer(keyedBy: CurrentlyWeatherCodingKeys.self, forKey: .currently)
+        let dailyWeather = try topLevelValues.nestedContainer(keyedBy: DailyWeatherCodingKeys.self, forKey: .dailyData)
         
         latitude = try topLevelValues.decode(Double.self, forKey: .latitude)
         longitude = try topLevelValues.decode(Double.self, forKey: .longitude)
@@ -47,5 +53,25 @@ extension WeatherData: Decodable {
         
         temperature = try currentlyWeather.decode(Double.self, forKey: .temperature)
         iconName = try currentlyWeather.decode(String.self, forKey: .iconName)
+        
+        dailyData = try dailyWeather.decode([DailyWeatherData].self, forKey: .data)
     }
 }
+
+struct DailyWeatherData: Decodable {
+    let time: Date
+    let iconName: String
+    let windSpeed: Double
+    let temperatureMin: Double
+    let temperatureMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case time
+        case iconName = "icon"
+        case windSpeed
+        case temperatureMin
+        case temperatureMax
+    }
+}
+
+
